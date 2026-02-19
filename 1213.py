33e3d2e357fa6204d9721159c8340abefd8e962a
ZanderBBlack <zanderblk4404@gmail.com>
2026-02-19 13:54:03 -0600

    Updated 2026-02-19 1:54pm

---
+++ 1213.py
@@ -0,0 +1,7 @@
+import platform
+platform_name = platform.platform()
+print(platform_name)
+
+arch_info = platform.architecture()
+
+print(arch_info)
--- Personal Budget Tracker ZB.py
+++ Personal Budget Tracker ZB.py
@@ -5,13 +5,24 @@ menu = [
     "4. Set Budget",
     "5. View Budget Summary",
     "6. Generate Report",
-    "7. Save & Exit",
+    "7. Save & Exit"
 ]
 
+
+expense_categories = [
+    "Food",
+    "Transportation",
+    "Entertainment",
+    "Bills",
+    "Other",
+]
+
+
 income_entries = {}
-expense_entries = {}
+expense_entries = {cat: [] for cat in expense_categories}
 
 def add_income():
+    print("\n----- Add Income -----")
     income = input("Enter Income Source:")
     
     if income in income_entries:
@@ -22,44 +33,67 @@ def add_income():
     print("Income added successfully.")
     
 def add_expense():
-    expense = input("Enter Expense Source:")
+    print("\n----- Add Expense -----")
+    print("Select Cateogry:")
+    for i, category in enumerate(expense_categories, 1):
+        print(f"{i}. {category}")
+    choice = input("Enter category (1-5): ")
     
-    if expense in expense_entries:
-        print("Expense source already exists.")
-        return
-    amount = float(input("Enter Amount:"))
-    expense_entries[expense] = amount
-    print("Expense added successfully.")
+    try:
+        idx = int(choice) - 1
+        if 0 <=  idx < len(expense_categories):
+            category_name = expense_categories[idx]
+            descript = input("Enter expense description:")
+            try:
+                amount = float(input("Enter Amount:"))
+            except ValueError:
+                print("Invalid amount. Must enter number")
+                return
+            expense_entries[category_name].append({
+            "description": descript,
+            "amount": amount
+            })
+            print(f"Expense added successfully. ${amount:.2f} to {category_name}.")
+        else:
+            print("Invalid range")
+            
+    except ValueError:
+        print("Invalid input. Please enter numbers only.")
+
+def view_transactions():
+    1 + 1
+def set_budget():
+    1 + 1
+def view_summary():
+    1 + 1
+def generate_report():
+    1 + 1
+
     
+
+actions = {
+    "1": add_income,
+    "2": add_expense,
+    "3": view_transactions,
+    "4": set_budget,
+    "5": view_summary,
+    "6": generate_report
+}
     
 while True:
     print ("\n--------- Personal Budget Tracker ----------")
-    for option in menu:
-        print(option)
-
-    choice = input("Choose an option (1-7):")
-
-    if choice == "1":
-        add_income()
+    print("\n".join(menu))
     
-    elif choice == "2":
-        add_expense()
-    
-    elif choice == "3":
-        view_transactions()
+    choice = input("Choose an option (1-7): ")
 
-    elif choice == "4":
-        set_budget()
-        
-    elif choice == "5":
-        view_summary()
-        
-    elif choice == "6":
-        generate_report()
-
-    elif choice == "7":
+    if choice == "7":
         print("Saving and exiting program.")
         break
-
+    
+    if choice in actions:
+        actions[choice]()
     else:
         print("Invalid option.")
+
+    input("\nPress Enter to Continue...")
+    
