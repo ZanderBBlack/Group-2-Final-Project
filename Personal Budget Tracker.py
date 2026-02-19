63d3e51e74d8a5558a2dea79352802e39d1ddaa8
LiamWalter <262650289+LiamWalter@users.noreply.github.com>
2026-02-19 14:52:05 -0600

    Added main logic.

--- Personal Budget Tracker.py
+++ Personal Budget Tracker.py
@@ -1,46 +1,66 @@
 class Main:
 
+
+    bankDetails = {
+        "Income": 0,
+        "Expense": 0,
+        "Transactions": "",
+        "Budget": 0,
+    }
+
+
     @staticmethod
     def Menu():
         print("""
-        [1] Add Income
-        [2] Add Expense
-        [3] View All Transactions
-        [4] Set Budget
-        [5] View Budget Summary
-        [6] Generate Report
-        [exit] Save And Exit
-        """)
+[1] Add Income
+[2] Add Expense
+[3] Add Transaction
+[4] View All Transactions
+[5] Set Budget
+[6] View Budget Summary
+[7] Generate Report
+[exit] Save And Exit
+""")
+
 
     @staticmethod
     def HandleInput():
-        selection = input("> ")
+        while True:
+            Main.Menu()
+            selection = input("> ")
+            details = Main.bankDetails
 
-        match selection:
-            case "1":
-                return "Add Income Selected"
-            
-            case "2":
-                return "Add Expense Selected"
-            
-            case "3":
-                return "View All Transactions Selected"
-            
-            case "4":
-                return "Set Budget Selected"
-            
-            case "5":
-                return "View Budget Summary Selected"
-            
-            case "6":
-                return "Generate Report Selected"
-            
-            case "exit":
-                return "Exiting..."
-            
-            case _:
-                return "Invalid Selection"
+            match selection:
+                case "1":
+                    details["Income"] = float(input("Income: "))
+                    print(f"Current Income: {details['Income']}")
+
+                case "2":
+                    details["Expense"] = float(input("Expense: "))
+                    print(f"Current Income: {details['Expense']}")
+
+                case "3":
+                    details["Transactions"] += input("Details: ")
+                    print(f"Transactions: {details['Transactions']}, ")
+
+                case "4":
+                    print(f"Transactions: {details['Transactions']}, ")
+
+                case "5":
+                    details["Budget"] += input("Budget: ")
+                    print(f"Budget: {details['Budget']}, ")
+
+                case "6":
+                    print("View Budget Summary Selected")
+
+                case "7":
+                    print("Generate Report Selected")
+
+                case "exit":
+                    print("Exiting...")
+                    break
+                case _:
+                    print("Invalid Selection")
 
 
-Main.Menu()
-print(Main.HandleInput())
+Main.HandleInput()
